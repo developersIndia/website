efa83da906d9c0113076567b104199181d94a978
Ashish Kumar <ashish.kmr@outlook.com>
2022-01-18 23:36:25 +0530

    remove un-necessary files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,7 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Developers India</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>Hello World</h1>
-      </main>
+      <h1> Hello from Developers India</h1>
     </div>
   );
 }

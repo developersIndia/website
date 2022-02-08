3201ac8eaadc091ddf2a9c6ca26ad9163281c3e9
Pushpender Saini <54404738+PushpenderSaini0@users.noreply.github.com>
2022-02-08 22:56:57 +0530

    Update app.tsx to use paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "../styles/globals.css";
+import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

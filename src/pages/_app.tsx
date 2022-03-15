48f5322e727a2da39ddc3c9477f1d60e1e6de313
DevanshBajaj <devansh21db.db@gmail.com>
2022-03-15 23:09:39 +0530

    chore: setup  NextUI in the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
-
+import { NextUIProvider } from "@nextui-org/react";
 import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
 export default MyApp;

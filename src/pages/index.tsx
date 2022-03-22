8a82c2d4652a50e968a4e2655dc3a9f934645f30
DevanshBajaj <devansh21db.db@gmail.com>
2022-03-22 14:54:34 +0530

    feat: add hero component in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
-import { Text } from "@nextui-org/react";
+// import { Text } from "@nextui-org/react";
+import Hero from "src/components/hero/Hero";
 
 export default function Home() {
   return (
     <div>
-      {/* Text is a NextUI component added to test the theme.ts file and for preview */}
-      <Text
-        h1
-        size={42}
-        css={{
-          //myGradient is declared in @styles/theme.ts
-          textGradient: "$myGradient",
-        }}>
-        Hello from Developers India
-      </Text>
+      {/* hero component */}
+      <Hero />
     </div>
   );
 }

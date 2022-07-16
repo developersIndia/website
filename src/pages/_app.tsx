790c2088a55c132e53ab8924e59d6f0c6777b5c9
Somraj Saha <somraj.1994@gmail.com>
2022-07-16 14:59:02 +0530

    remove some redundancy from the code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
 import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
-import "@styles/globals.css";
-import { lightTheme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    //theme props are passsed here to apply theme to the whole website.
-    <NextUIProvider theme={lightTheme}>
-      <Component {...pageProps} />
-    </NextUIProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import { Text } from "@nextui-org/react";
-
-export default function Home() {
-  return (
-    <div>
-      {/* Text is a NextUI component added to test the theme.ts file and for preview */}
-      <Text
-        h1
-        size={42}
-        css={{
-          //myGradient is declared in @styles/theme.ts
-          textGradient: "$myGradient",
-        }}>
-        Hello from Developers India
-      </Text>
-    </div>
-  );
+function IndexPage() {
+  return <>Hello World!</>;
 }
+
+export default IndexPage;

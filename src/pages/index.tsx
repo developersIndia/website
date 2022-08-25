83e55be4fa6328a8fb58753ca10d31aeb4035808
Somraj Saha <somraj.1994@gmail.com>
2022-08-25 16:58:33 +0530

    Add the necessary packages for styling the `index` page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,24 @@
-import { Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import {
+  BsCurrencyDollar,
+  BsFillLightningChargeFill,
+  BsGraphUp,
+  BsHeartFill,
+} from "react-icons/bs";
+import { FaDiscord, FaGithub, FaReddit } from "react-icons/fa";
 
 function IndexPage() {
   return <Text>Hello from the DevelopersIndia Community!</Text>;

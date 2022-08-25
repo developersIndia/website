19a5c72afb2cff00edb2a2bab26cc556521d765d
Somraj Saha <somraj.1994@gmail.com>
2022-08-25 22:50:04 +0530

    Add a `_document.tsx` file for more concrete SEO handling

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+function Document() {
+  return (
+    <Html lang="en">
+      <title>The DevelopersIndia Community</title>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'"
+        />
+        <meta
+          name="description"
+          content="The largest network for software & tech professions in India."
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;

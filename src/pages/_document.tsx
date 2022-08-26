4f4d5ed6e5aaf8ca77bf4a5699612f2822d961bb
Somraj Saha <somraj.1994@gmail.com>
2022-08-26 14:37:33 +0530

    Add stuff for the favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,14 @@ function Document() {
           name="description"
           content="The largest network for software & tech professions in India."
         />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ce7546" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        {/* TODO: Add other stuff like Google Analytics & Search tag */}
       </Head>
       <body>
         <Main />

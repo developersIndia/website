e3e7b9e6f0f30f9f1840c06a4d6b24d0f70e7675
Somraj Saha <somraj.1994@gmail.com>
2022-08-28 00:13:23 +0530

    adjust the margins a bit to make the section look nicer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,21 +1,19 @@
 import SocialButtons from "../social-buttons";
-import { Center, Heading, Stack, Text } from "@chakra-ui/react";
+import { Center, Heading, Text } from "@chakra-ui/react";
 
 function Header() {
   return (
-    <Stack direction="column" spacing={6}>
+    <>
       <Heading
         size={{ base: "xl", md: "2xl" }}
         fontWeight="extrabold"
         textAlign="center"
+        marginBottom={8}
       >
         India&apos;s Largest Network of Software & Technical Professionals
       </Heading>
 
-      {/* <Text paddingX={60}> */}
-      {/* </Text> */}
-
-      <Text textAlign="center" paddingX={{ md: 32 }}>
+      <Text textAlign="center" marginBottom={8} paddingX={{ md: 32 }}>
         We&apos;re India&apos;s biggest & ever-fastest growing community of
         software developers & tech professionals! Join us & hang around with us
         on Discord, Reddit and/or on GitHub.
@@ -24,7 +22,7 @@ function Header() {
       <Center paddingBottom={12}>
         <SocialButtons />
       </Center>
-    </Stack>
+    </>
   );
 }
 

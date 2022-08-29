9a45e0a2fbbe0a278d58be872008250781fcd912
Somraj Saha <somraj.1994@gmail.com>
2022-08-29 16:46:10 +0530

    customise the aesthetics of the CTA section with better fonts

--- src/components/cta/index.tsx
+++ src/components/cta/index.tsx
@@ -1,12 +1,27 @@
 import SocialButtons from "../social-buttons";
-import { Center, Heading } from "@chakra-ui/react";
+import { Center, Heading, Text } from "@chakra-ui/react";
 
 function CtaSection() {
   return (
     <>
-      <Heading size="lg" textAlign="center" marginY={6}>
+      <Heading
+        fontFamily="inter"
+        fontWeight="bold"
+        size="lg"
+        textAlign="center"
+        marginY={6}
+      >
         &gt;Ready to be Part of the Community?
       </Heading>
+      <Text
+        fontFamily="inter"
+        paddingX={{ base: 12 }}
+        paddingBottom={10}
+        textAlign="center"
+        color="#667085"
+      >
+        Join us in our attempt to make the community a better place.
+      </Text>
       <Center>
         <SocialButtons />
       </Center>

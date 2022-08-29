af6842766a00dfa01e32c44aa0e778c1572cb90a
Somraj Saha <somraj.1994@gmail.com>
2022-08-29 16:48:21 +0530

    customised the fonts of the header section

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,19 +1,25 @@
 import SocialButtons from "../social-buttons";
 import { Center, Heading, Text } from "@chakra-ui/react";
+import "@fontsource/inter";
 
 function Header() {
   return (
     <>
       <Heading
         size={{ base: "xl", md: "2xl" }}
-        fontWeight="extrabold"
         textAlign="center"
         marginBottom={8}
+        fontFamily="inter"
       >
         India&apos;s Largest Network of Software & Technical Professionals
       </Heading>
 
-      <Text textAlign="center" marginBottom={8} paddingX={{ md: 32 }}>
+      <Text
+        textAlign="center"
+        marginBottom={8}
+        paddingX={{ md: 32 }}
+        fontFamily="inter"
+      >
         We&apos;re India&apos;s biggest & ever-fastest growing community of
         software developers & tech professionals! Join us & hang around with us
         on Discord, Reddit and/or on GitHub.

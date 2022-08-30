ecbf8129315511dfcbd383ef6375b062434ba68d
Somraj Saha <somraj.1994@gmail.com>
2022-08-30 15:25:01 +0530

    refactored the constants just enough for the future

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,12 @@
+// INFO: Types of Social Media (SM) profiles each moderation can have.
+type SmTypes = {
+  twitter?: string;
+  linkedin?: string;
+  github?: string;
+  website?: string;
+};
+
+// INFO: Strict "types" for better management of displaying the moderation team information.
 type ModeratorTypes = {
   name:
     | "Somraj Saha"
@@ -5,44 +14,70 @@ type ModeratorTypes = {
     | "Sidharth Gehlot"
     | "Yash Sharan"
     | "Bhupesh Pradhan"
+    | "Namanpreth Shergill"
+    | "Placeholder John"
     | "Shreyas Gupta";
-  designation:
+  role:
     | "Community Admin"
     | "GitHub Moderator"
     | "Discord Moderator"
     | "SubReddit Moderator";
   img: string;
   tagline: string;
-  twitter?: string;
-  linkedin?: string;
-  github?: string;
-  website?: string;
+  socials?: SmTypes;
 };
 
 export const moderators: ModeratorTypes[] = [
   {
-    name: "Somraj Saha",
-    designation: "GitHub Moderator",
-    img: "https://avatars.githubusercontent.com/u/31373860?v=4",
-    tagline: "Placeholder text to check if TypeScript works.",
-    twitter: "Jarmosan",
-    linkedin: "jarmos",
-    github: "Jarmos-san",
-    website: "jarmos.vercel.app",
+    name: "Sidharth Gehlot",
+    role: "Community Admin",
+    img: "https://avatars.githubusercontent.com/u/54112684?v=4",
+    tagline: "Self-employed entrepreneur with lots & lots of talents!",
+    socials: {
+      github: "sidharthgehlot",
+    },
   },
   {
     name: "Bhupesh Varshney",
-    designation: "Community Admin",
+    role: "Community Admin",
     img: "https://avatars.githubusercontent.com/u/34342551?v=4",
     tagline: "World's best community admin!",
-    twitter: "bhupeshhimself",
-    github: "https://github.com/Bhupesh-V",
+    socials: {
+      twitter: "bhupeshhimself",
+      github: "Bhupesh-V",
+    },
   },
   {
-    name: "Sidharth Gehlot",
-    designation: "Community Admin",
-    img: "https://avatars.githubusercontent.com/u/54112684?v=4",
-    tagline: "Self-employed entrepreneur with lots & lots of talents!",
-    github: "https://github.com/sidharthgehlot",
+    name: "Somraj Saha",
+    role: "GitHub Moderator",
+    img: "https://avatars.githubusercontent.com/u/31373860?v=4",
+    tagline: "Placeholder text to check if TypeScript works.",
+    socials: {
+      twitter: "Jarmosan",
+      linkedin: "jarmos",
+      github: "Jarmos-san",
+      website: "jarmos.vercel.app",
+    },
+  },
+  {
+    name: "Placeholder John",
+    role: "Discord Moderator",
+    img: "https://picsum.photos/200/300?random=1",
+    tagline:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis pharetra dui. Donec imperdiet velit.",
+  },
+  {
+    name: "Placeholder John",
+    role: "Discord Moderator",
+    img: "https://picsum.photos/200/300?random=2",
+    tagline:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis pharetra dui. Donec imperdiet velit.",
+  },
+  {
+    name: "Placeholder John",
+    role: "Discord Moderator",
+    img: "https://picsum.photos/200/300?random=3",
+    tagline:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis pharetra dui. Donec imperdiet velit.",
   },
 ];
